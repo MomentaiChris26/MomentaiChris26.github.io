ac3b553213b1e82cdabe6748c5448b0821da1d19
Chris-Tri <christophertri90@gmail.com>
2021-08-30 17:12:32 +1000

    clean up

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,52 +1,92 @@
 import React, { useState } from "react";
 import { Link } from "gatsby";
+import { useMediaQuery } from "react-responsive";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
-const activeLinkClass = "border-b-2 border-jordyBlue-600";
+const styleClasses = {
+  active: "border-b-2 border-jordyBlue-600",
+  mobLink:
+    "block py-2 px-4 text-navText text-md hover:bg-red-500 font-roboto-mono",
+  hoverLink: "hover:bg-red-500 text-lg",
+};
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMediaQueryChange = (matches) => {
+    if (!matches) {
+      setMenuOpen(false);
+    }
+  };
+
+  const isTabletOrMobile = useMediaQuery(
+    { maxWidth: 768 },
+    undefined,
+    handleMediaQueryChange
+  );
+
   return (
     <nav className="px-4 bg-opacity-0 text-navText">
-      <div className="max-w mx-auto ">
-        <div className="flex justify-between py-4 font-roboto-mono">
+      {/* function to tell the app if the view is mobile */}
+      {isTabletOrMobile}
+      <div className="max-w mx-auto">
+        <div className="flex items-center justify-between py-4 font-roboto-mono">
           <Link className="text-3xl text-navText hover:bg-red-500" to="/">
             Chris
           </Link>
-          <div className="hidden md:flex items-center text-lg space-x-6 ">
+          <div className="hidden md:flex items-center space-x-6 ">
             <Link
               to="/profile"
-              className="hover:bg-red-500"
-              activeClassName={activeLinkClass}
+              className={styleClasses.hoverLink}
+              activeClassName={styleClasses.active}
             >
               Profile
             </Link>
             <Link
               to="/projects"
-              className="hover:bg-red-500"
-              activeClassName={activeLinkClass}
+              className={styleClasses.hoverLink}
+              activeClassName={styleClasses.active}
             >
               Projects
             </Link>
-            <button onClick={() => console.log("resume link")}>Resume</button>
+            <a
+              className="py-2 px-4 rounded bg-gray-500 border border-white hover:bg-white hover:border-gray-500 focus:outline-none"
+              href={"/public/static/documents/chris_tri_cv.pdf"}
+              alt="downlod-resume"
+              download="Christopher Tri - CV.pdf"
+            >
+              Download Resume
+            </a>
           </div>
           <div className="md:hidden flex items-center">
-            <button onClick={() => setMenuOpen(!menuOpen)}>Menu</button>
+            <FontAwesomeIcon
+              icon={menuOpen ? faTimes : faBars}
+              size="2x"
+              className="cursor-pointer"
+              onClick={() => {
+                setMenuOpen(!menuOpen);
+              }}
+            />
           </div>
         </div>
       </div>
+
       <div className={menuOpen ? null : "hidden"}>
-        <Link
-          to="/profile"
-          className="block py-2 px-4 text-sm hover:bg-red-500"
-        >
+        <Link to="/profile" className={styleClasses.mobLink}>
           Profile
         </Link>
-        <Link
-          to="/projects"
-          className="block py-2 px-4 text-sm hover:bg-red-500"
-        >
+        <Link to="/projects" className={styleClasses.mobLink}>
           Projects
         </Link>
+        <a
+          className={`${styleClasses.mobLink} + cursor-pointer`}
+          href={"/public/static/documents/chris_tri_cv.pdf"}
+          alt="downlod-resume"
+          download="Christopher Tri - CV.pdf"
+        >
+          Download Resume
+        </a>
       </div>
     </nav>
   );
