ca3712d820b822977770d7f17c7231ed91ade5c7
Chris-Tri <christophertri90@gmail.com>
2021-09-06 21:26:41 +1000

    updated home page with links and fix mobile view

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,10 @@
 import * as React from "react";
 import DefaultLayout from "../components/layout";
-import Navbar from "../components/Navbar/Navbar";
 
 const ProjectsPage = () => {
   return (
     <DefaultLayout>
       <div className="bg-projectPage flex-grow">
-        <Navbar />
         <h1>Projects</h1>
       </div>
     </DefaultLayout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "media", // or 'media' or 'class' or false
   theme: {
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
     backgroundColor: (theme) => ({
       ...theme("colors"),
       homePage: "#19346B",
