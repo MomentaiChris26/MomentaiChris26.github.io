666bba2e6cb9a6ce8919ab72d6577b31700811b6
Chris-Tri <christophertri90@gmail.com>
2021-11-10 16:36:36 +1100

    updates

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,7 +5,10 @@ const ProjectsPage = () => {
   return (
     <DefaultLayout>
       <div className="bg-projectPage flex-grow">
-        <h1>Projects</h1>
+        <div className="flex h-4/5 items-center flex-col my-2">
+          <h1 className="my-10 text-3xl text-white">Projects</h1>
+          <div className="w-4/5 flex-grow bg-red-400">places</div>
+        </div>
       </div>
     </DefaultLayout>
   );
